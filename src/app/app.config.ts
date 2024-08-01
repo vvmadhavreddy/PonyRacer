821a40c7afa085d2713a3d8cf9477897eb607f39
Venu <vvmadhavreddy@gmail.com>
2024-08-01 20:46:11 +0530

    Bringing races data into the RaceService from the API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient()]
 };
